be0ec292c34e99b37076910cbec75e0c220a228c
Simon Szabo <szabo.simon@cae.com>
2024-01-27 16:30:47 +0100

    Initial commit

---
+++ src/main/java/sales/sale_taxes_be/SaleTaxesBeApplication.java
@@ -0,0 +1,13 @@
+package sales.sale_taxes_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleTaxesBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleTaxesBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sales/sale_taxes_be/SaleTaxesBeApplicationTests.java
@@ -0,0 +1,13 @@
+package sales.sale_taxes_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleTaxesBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
